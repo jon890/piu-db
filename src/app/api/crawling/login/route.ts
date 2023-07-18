e70577a5bb3420042b99ccf64cef4809604f1433
BiFoS <jon89071@gmail.com>
2023-07-18 20:31:35 +0900

    :art: 크롤러 구조 변경

--- src/app/api/crawling/login/route.ts
+++ src/app/api/crawling/login/route.ts
@@ -18,11 +18,11 @@ const LoginParams = z.object({
 
 export async function POST(request: NextRequest) {
   const reqBody = (await request.json()) as unknown;
-  await handleAmPassLogin(LoginParams.parse(reqBody));
+  const bestScore = await handleAmPassLogin(LoginParams.parse(reqBody));
 
   return NextResponse.json({
     successful: true,
-    data: {},
+    data: { bestScore },
   });
 }
 
@@ -52,10 +52,12 @@ async function handleAmPassLogin(params: LoginParams) {
 
   console.debug("login checked");
 
-  await getBestScore(browser);
+  const bestScore = await getBestScore(browser);
 
   await page.close();
   await browser.close();
+
+  return bestScore;
 }
 
 async function getBestScore(browser: Browser) {
@@ -74,11 +76,36 @@ async function getBestScore(browser: Browser) {
   await page.goto("https://www.piugame.com/my_page/my_best_score.php");
   console.debug("move to my best score page");
 
-  const data = await page.$$eval("ul.my_best_scoreList li", (lis) =>
-    lis.map((it) => it.innerHTML)
+  const data = await page.$$eval("ul.my_best_scoreList > li", (lis) =>
+    lis.map((it) => {
+      const type = it.querySelector("div.tw img"); // single or double
+      const isDouble = (type as HTMLImageElement)?.src?.includes("d_text.png");
+      const isSingle = (type as HTMLImageElement)?.src?.includes("s_text.png");
+
+      const levelBalls = it.querySelectorAll("div.numw div.imG img");
+      const level = Array.from(levelBalls)
+        .map((it) => {
+          const img = it as HTMLImageElement;
+          const src = img?.src;
+          const splits = src.split("/");
+          const png = splits[splits.length - 1];
+          const [ball] = png.split(".");
+          const ballSplits = ball.split("_");
+          return ballSplits[ballSplits.length - 1];
+        })
+        .join("");
+
+      const songName = it.querySelector("div.song_name p")?.textContent;
+      const score = it.querySelector(
+        "div.li_in div.txt_v span.num"
+      )?.textContent;
+
+      return { isDouble, isSingle, level, songName, score };
+    })
   );
-  console.log(data);
 
   page.off("dialog", authFailedHandler);
   page.close();
+
+  return data;
 }
