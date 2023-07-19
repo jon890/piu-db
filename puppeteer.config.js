ced347d0892834d748c9d908de176806cc9b364d
BiFoS <jon89071@gmail.com>
2023-07-19 13:37:24 +0900

    :wrench: Update puppeteer config

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -9,8 +9,8 @@ module.exports = {
   logLevel: "warn",
   defaultProduct: "chrome",
   browserRevision: "114.0.5735.133",
-  ...(process?.env?.NODE_ENV === "production" && {
+  ...(process?.env?.NODE_ENV !== "production" && {
     executablePath:
-      "/home/jon89071/piu-db/puppeteer/chrome/linux64-114.0.5735.133/chrome-linux64",
+      "/home/jon89071/piu-db/puppeteer/chrome/linux64-114.0.5735.133/chrome-linux64/chrome",
   }),
 };
