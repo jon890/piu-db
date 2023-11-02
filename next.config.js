3bca4c448dc283ba3ffec8f5691287d44ee8c5cf
BiFoS <jon89071@gmail.com>
2023-11-02 12:10:54 +0900

    :wrench: Cors: update cors config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const getCorsHeaders = () => {
+  const headers = {};
+  headers["Access-Control-Allow-Origin"] =
+    "http://localhost:3000,https://piudb.com,https://www.piudb.com";
+  headers["Access-Control-Allow-Credentials"] = "true";
+  headers["Access-Control-Allow-Methods"] = "GET,OPTIONS,PATCH,DELETE,POST,PUT";
+  headers["Access-Control-Allow-Headers"] =
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization";
+
+  return Object.entries(headers).map(([key, value]) => ({ key, value }));
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,6 +21,14 @@ const nextConfig = {
         pathname: "/photos/**",
       },
     ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: getCorsHeaders(),
+      },
+    ];
   },
 };
 
