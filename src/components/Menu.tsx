18983273045a5f2cb677c9efd2eae95255dc2b71
BiFoS <jon89071@gmail.com>
2024-02-21 18:41:05 +0900

    :lipstick: 메뉴 active 상태 추가

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,33 @@
+"use client";
+
+import classnames from "@/client/utils/classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+  const pathname = usePathname();
+
+  console.log("pathname", pathname);
+
   return (
-    <ul className="menu menu-xs sm:menu-sm md:menu-md lg:menu-lg p-4 w-80 min-h-full bg-base-200 font-semibold space-y-5">
+    <ul className="menu menu-sm md:menu-md lg:menu-lg p-4 w-40 lg:w-60 xl:w-80 min-h-full bg-base-200 font-semibold gap-5">
       <li>
-        <Link href="/rooms">방 목록</Link>
+        <Link
+          href="/rooms"
+          className={classnames(pathname.startsWith("/rooms") ? "active" : "")}
+        >
+          방 목록
+        </Link>
       </li>
       <li>
-        <Link href="/crawling">펌프잇업 기록 연동</Link>
+        <Link
+          href="/crawling"
+          className={classnames(
+            pathname.startsWith("/crawling") ? "active" : ""
+          )}
+        >
+          펌프잇업 기록 연동
+        </Link>
       </li>
     </ul>
   );
