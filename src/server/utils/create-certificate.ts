0eccb90e36b40e781a1b549a202b4f74ce0a06fa
BiFoS <jon89071@gmail.com>
2024-02-21 19:05:05 +0900

    :green_heart: 빌드 오류 수정

--- src/server/utils/create-certificate.ts
+++ src/server/utils/create-certificate.ts
@@ -1,14 +1,11 @@
 import fs from "fs";
 import os from "os";
 
-// console.log("tmpDir", os.tmpdir());
-// console.log("process.env", process.env.NODE_ENV);
-
 // nextjs dev server reloads files on page navigation, so new Prisma Clients were being spawned everytime
 export function createCertificate() {
-  console.log("createCertificate in ", os.tmpdir());
+  const tmpDir = os.hostname() === "kbt.local" ? "/Users/nhn" : os.tmpdir();
   const clientIdentity = process.env.SSL_IDENTITY ?? "";
   const decodeIdentity = Buffer.from(clientIdentity, "base64");
-  fs.writeFileSync(`${os.tmpdir()}/client-identity.p12`, decodeIdentity);
-  fs.writeFileSync(`${os.tmpdir()}/server-ca.pem`, process.env.SSL_CERT ?? "");
+  fs.writeFileSync(`${tmpDir}/client-identity.p12`, decodeIdentity);
+  fs.writeFileSync(`${tmpDir}/server-ca.pem`, process.env.SSL_CERT ?? "");
 }
