4cbedc561d532a8036121a8ca5adce74b7cedc28
BiFoS <jon89071@gmail.com>
2024-03-04 18:34:33 +0900

    :lipstick: Navbar 수정

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,13 @@
 import { auth, signOut } from "@/auth";
 import Link from "next/link";
+import Bars3Icon from "@heroicons/react/24/solid/Bars3Icon";
 
 export default async function NavBar() {
   const session = await auth();
+
+  if (!session?.user) {
+    return null;
+  }
 
   return (
     <div className="w-full navbar bg-base-100">
@@ -12,19 +17,7 @@ export default async function NavBar() {
           aria-label="open sidebar"
           className="btn btn-square btn-ghost"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-6 h-6 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+          <Bars3Icon className="size-6" />
         </label>
       </div>
 
@@ -34,32 +27,15 @@ export default async function NavBar() {
         </Link>
       </div>
 
-      {session?.user && (
-        <div className="flex-none">
-          <span>{session.user.name}</span>
-        </div>
-      )}
-
       <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-full h-full"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
+        <div className="dropdown dropdown-end dropdown-bottom">
+          <button tabIndex={0} className="btn btn-primary btn-ghost">
+            <div className="size-8 rounded-full bg-green-500 flex items-center justify-center">
+              {session.user.name?.charAt(0)}
             </div>
-          </label>
+
+            <span>{session.user.name}</span>
+          </button>
 
           <ul
             tabIndex={0}
