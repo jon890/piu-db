5a0898846f367787410b701aead330c30b1b46c6
BiFoS <jon89071@gmail.com>
2024-03-05 09:28:55 +0900

    :sparkles: 임시 캐쉬 리프레시 기능 추가

---
+++ src/app/(app)/api/refresh-cache/route.ts
@@ -0,0 +1,21 @@
+import { auth } from "@/auth";
+import { initializeCache } from "@/server/prisma/init";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const session = await auth();
+  const id = session?.user?.name;
+
+  if (!id) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  if (id !== "bifos") {
+    return NextResponse.json({ error: "Not permitted" }, { status: 403 });
+  }
+
+  await initializeCache();
+  console.log("Cache refreshed!! by", id);
+
+  return NextResponse.json({ message: "success" }, { status: 200 });
+}
--- src/instrumentation.ts
+++
@@ -1,6 +0,0 @@
-import { initializeCache } from "./server/prisma/init";
-
-export async function register() {
-  console.log("instrumentation register");
-  initializeCache();
-}
