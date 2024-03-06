7f51a54396014ef5f8e21fc6020e5c517a964335
BiFoS <jon89071@gmail.com>
2024-03-06 14:09:02 +0900

    :fire: Remove cloudbuild config

--- prisma/migrations/20240304054916_add_played_time/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `playedAt` to the `td_record` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "td_record" ADD COLUMN     "playedAt" TIMESTAMP(3) NOT NULL;
