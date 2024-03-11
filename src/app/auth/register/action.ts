44d062a06612c00d5006be31fbdba99179b3a9c5
BiFoS <jon89071@gmail.com>
2024-03-11 17:55:18 +0900

    :art: 유저에 uid 기능 추가

--- src/app/auth/register/action.ts
+++ src/app/auth/register/action.ts
@@ -28,13 +28,28 @@ export async function registerUser(
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const user = await prisma.user.create({
+  // todo uid 중복시 재시도?
+  await prisma.user.create({
     data: {
       name,
       password: hashedPassword,
       nickname,
+      uid: getRandomUID(6),
     },
   });
 
   redirect("/auth/login");
+}
+
+function getRandomUID(length: number) {
+  const candidates =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
+
+  let uid = "";
+  for (let i = 0; i < length; i++) {
+    const index = Math.floor(Math.random() * candidates.length);
+    uid += candidates[index];
+  }
+
+  return uid;
 }
