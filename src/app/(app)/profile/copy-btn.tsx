1866b6945cfd4c542ee99abe6d49c88b7facc03f
BiFoS <jon89071@gmail.com>
2024-03-12 11:58:29 +0900

    :art: 복사 테스ㅡㅌ

--- src/app/(app)/profile/copy-btn.tsx
+++ src/app/(app)/profile/copy-btn.tsx
@@ -5,9 +5,13 @@ type Props = {
 };
 
 export default function CopyButton({ text }: Props) {
-  function handleCopy() {
-    window.navigator.clipboard.writeText(text);
-    alert("UID가 복사되었습니다! 친구에게 공유해보세요");
+  async function handleCopy() {
+    try {
+      await window.navigator.clipboard.writeText(text);
+      alert("UID가 복사되었습니다! 친구에게 공유해보세요");
+    } catch (e) {
+      alert("에러가 발생했습니다." + JSON.stringify(e));
+    }
   }
 
   return (
