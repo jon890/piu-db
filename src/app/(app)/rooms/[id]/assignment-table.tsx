422230c7a9862250011cf6a30545592ce0ca4cf2
BiFoS <jon89071@gmail.com>
2024-03-18 15:55:44 +0900

    :lipstick: 숙제목록 UI 변경

--- src/app/(app)/rooms/[id]/assignment-table.tsx
+++ src/app/(app)/rooms/[id]/assignment-table.tsx
@@ -27,6 +27,7 @@ export default async function AssignmentTable({
       assignment,
       song: songAndChart?.song,
       chart: songAndChart?.chart,
+      endDateDiff: dayjs(assignment.endDate).diff(dayjs(), "hours"),
     });
   }
 
@@ -50,50 +51,82 @@ export default async function AssignmentTable({
             <th></th>
             <th>곡이름</th>
             <th>레벨/SD</th>
-            <th>시작일</th>
+            {!onGoing && <th>시작일</th>}
             <th>종료일</th>
-            <th>상태</th>
+            {!onGoing && <th>상태</th>}
             <th>등록일</th>
             <th>등록자</th>
           </tr>
         </thead>
         <tbody>
-          {assignmentWithSong?.map(({ assignment, chart, song }, index) => (
-            <tr
-              key={index}
-              className="*:text-xs *:px-2 *:py-1 *:md:text-sm *:md:px-4 *:md:py-2 hover"
-            >
-              <td>{index + 1}</td>
-              <td>
-                <Link
-                  href={`/rooms/${room.seq}/assignments/${assignment.seq}`}
-                  className="hover:text-gray-600 font-semibold"
-                >
-                  {song?.name}
-                </Link>
-              </td>
-              <td>
-                {chart?.chartType && (
-                  <LevelBallSC chart={chart} className="size-8" />
+          {assignmentWithSong?.map(
+            ({ assignment, chart, song, endDateDiff }, index) => (
+              <tr
+                key={index}
+                className="*:text-xs *:px-2 *:py-1 *:md:text-sm *:md:px-4 *:md:py-2 hover"
+              >
+                <td>{index + 1}</td>
+                <td>
+                  <Link
+                    href={`/rooms/${room.seq}/assignments/${assignment.seq}`}
+                    className="hover:text-gray-600 font-semibold"
+                  >
+                    {song?.name}
+                  </Link>
+                </td>
+                <td>
+                  {chart?.chartType && (
+                    <LevelBallSC chart={chart} className="size-8" />
+                  )}
+                </td>
+
+                {!onGoing && (
+                  <td>
+                    {TimeUtil.format(assignment.startDate, "YYYY-MM-DD HH:mm")}
+                  </td>
                 )}
-              </td>
-              <td>
-                {TimeUtil.format(assignment.startDate, "YYYY-MM-DD HH:mm")}
-              </td>
-              <td>{TimeUtil.format(assignment.endDate, "YYYY-MM-DD HH:mm")}</td>
-              <td>
-                {dayjs().isBefore(assignment.endDate) ? (
-                  <span className="text-blue-500 font-semibold">진행 중</span>
+
+                {onGoing ? (
+                  <td className="space-x-2">
+                    <span>
+                      {TimeUtil.format(assignment.endDate, "YYYY-MM-DD HH:mm")}
+                    </span>
+
+                    {Math.floor(endDateDiff / 24) > 0 ? (
+                      <span className="text-blue-500 font-semibold">
+                        {Math.floor(endDateDiff / 24)}일 남음
+                      </span>
+                    ) : (
+                      <span className="text-orange-500 font-semibold">
+                        {endDateDiff}시간 남음
+                      </span>
+                    )}
+                  </td>
                 ) : (
-                  <span className="text-gray-500">종료</span>
+                  <td>
+                    {TimeUtil.format(assignment.endDate, "YYYY-MM-DD HH:mm")}
+                  </td>
                 )}
-              </td>
-              <td>
-                {TimeUtil.format(assignment.createdAt, "YYYY-MM-DD HH:mm")}
-              </td>
-              <td>{assignment.createUser.nickname}</td>
-            </tr>
-          ))}
+
+                {!onGoing && (
+                  <td>
+                    {dayjs().isBefore(assignment.endDate) ? (
+                      <span className="text-blue-500 font-semibold">
+                        진행 중
+                      </span>
+                    ) : (
+                      <span className="text-gray-500">종료</span>
+                    )}
+                  </td>
+                )}
+
+                <td>
+                  {TimeUtil.format(assignment.createdAt, "YYYY-MM-DD HH:mm")}
+                </td>
+                <td>{assignment.createUser.nickname}</td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </div>
