df37f7d56469e362d60144858ba12da8d7ab005b
BiFoS <jon89071@gmail.com>
2024-03-22 15:26:24 +0900

    :lipstick: 점수를 조금 더 예쁘게 표현

--- src/app/(app)/skill-attack/page.tsx
+++ src/app/(app)/skill-attack/page.tsx
@@ -17,7 +17,16 @@ export default async function SkillAttackPage() {
     <ContentBox title="스킬 어택">
       <SkillAttackButton />
 
-      <p>총점 : {skillAttack?.skillPoints.toNumber() ?? 0}</p>
+      {skillAttack ? (
+        <p>
+          Your Skill Points
+          <strong className="ml-4 mt-32 bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text text-5xl font-bold text-transparent">
+            {skillAttack.skillPoints.toFixed(3)}
+          </strong>
+        </p>
+      ) : (
+        <p>위 버튼을 눌러 스킬 어택을 동기화 해주세요</p>
+      )}
 
       {records.length > 0 && <SkillAttackTable records={records} />}
     </ContentBox>
