5f4d541295ff63c086c4fe53ba04bc244d7ea946
BiFoS <jon89071@gmail.com>
2024-03-25 16:43:10 +0900

    :art: Navbar에서 로그인 정보 없을 시 분기를 수정

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -9,64 +9,64 @@ export default async function NavBar() {
   const userSeq = await AuthUtil.getUserSeqThrows();
   const user = await UserDB.getUserBySeq(userSeq);
 
+  if (!user) {
+    return (
+      <ServerToastHelper
+        toast={{ message: "게정 정보를 찾지 못했습니다", type: "error" }}
+        action="SIGN_OUT"
+      />
+    );
+  }
+
   return (
-    <>
-      <nav className="w-full navbar bg-base-100">
-        <div className="flex-none lg:hidden">
-          <label
-            htmlFor="my-drawer-3"
-            aria-label="open sidebar"
-            className="btn btn-square btn-ghost"
+    <nav className="w-full navbar bg-base-100">
+      <div className="flex-none lg:hidden">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="open sidebar"
+          className="btn btn-square btn-ghost"
+        >
+          <Bars3Icon className="size-6" />
+        </label>
+      </div>
+
+      <div className="flex-1">
+        <Link href="/rooms" className="btn btn-ghost normal-case text-xl">
+          PIU DB
+        </Link>
+      </div>
+
+      <div className="flex-none">
+        <div className="dropdown dropdown-end dropdown-bottom">
+          <button tabIndex={0} className="btn btn-primary btn-ghost">
+            <div className="size-8 rounded-full bg-green-500 flex items-center justify-center text-white dark:text-black">
+              {user.nickname.charAt(0)}
+            </div>
+
+            <span>{user.nickname}</span>
+          </button>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <Bars3Icon className="size-6" />
-          </label>
+            <li>
+              <Link href="/profile" className="justify-between">
+                프로필
+                <span className="badge">New</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button>로그아웃</button>
+              </form>
+            </li>
+          </ul>
         </div>
-
-        <div className="flex-1">
-          <Link href="/rooms" className="btn btn-ghost normal-case text-xl">
-            PIU DB
-          </Link>
-        </div>
-
-        <div className="flex-none">
-          <div className="dropdown dropdown-end dropdown-bottom">
-            <button tabIndex={0} className="btn btn-primary btn-ghost">
-              <div className="size-8 rounded-full bg-green-500 flex items-center justify-center text-white dark:text-black">
-                {user?.nickname?.charAt(0)}
-              </div>
-
-              <span>{user?.nickname}</span>
-            </button>
-
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <Link href="/profile" className="justify-between">
-                  프로필
-                  <span className="badge">New</span>
-                </Link>
-                <form
-                  action={async () => {
-                    "use server";
-                    await signOut();
-                  }}
-                >
-                  <button>로그아웃</button>
-                </form>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      {!user && (
-        <ServerToastHelper
-          toast={{ message: "게정 정보를 찾지 못했습니다", type: "error" }}
-          action="SIGN_OUT"
-        />
-      )}
-    </>
+      </div>
+    </nav>
   );
 }
