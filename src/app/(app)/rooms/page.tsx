99ae2252c0b1484aeb493fde8a571b1b51e5927c
BiFoS <jon89071@gmail.com>
2024-04-04 15:16:17 +0900

    :art: 숙제 목록 UI 로딩 수정

--- src/app/(app)/rooms/page.tsx
+++ src/app/(app)/rooms/page.tsx
@@ -2,25 +2,44 @@ import ContentBox from "@/components/layout/content-box";
 import RoomDB from "@/server/prisma/room.db";
 import Link from "next/link";
 import Room from "./room";
+import { Suspense } from "react";
 
 export default async function RoomListPage() {
-  const rooms = await RoomDB.getRooms();
-
   return (
     <ContentBox title="숙제방 목록">
       <Link href="/rooms/create" className="btn btn-primary text-xs sm:text-sm">
         숙제방 생성
       </Link>
 
-      <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-5 w-full">
-        {rooms.map((r) => (
-          <Room
-            key={r.seq}
-            room={r}
-            count={r._count.assignmentRoomParticipants}
-          />
-        ))}
-      </div>
+      <Suspense fallback={<RoomSkeleton />}>
+        <RoomList />
+      </Suspense>
     </ContentBox>
+  );
+}
+
+async function RoomList() {
+  const rooms = await RoomDB.getRooms();
+
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-5 w-full">
+      {rooms.map((r) => (
+        <Room
+          key={r.seq}
+          room={r}
+          count={r._count.assignmentRoomParticipants}
+        />
+      ))}
+    </div>
+  );
+}
+
+function RoomSkeleton() {
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-5 w-full">
+      {[...Array(10)].map((v, i) => (
+        <div className="skeleton w-full h-48" key={i}></div>
+      ))}
+    </div>
   );
 }
