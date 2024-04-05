a76470526cc52ebd88de6b9e86c0aa7738133d6b
BiFoS <jon89071@gmail.com>
2024-04-05 12:23:25 +0900

    :lipstick: 싱글 더블 구분자 추가

--- src/app/(app)/records/level/[level]/page.tsx
+++ src/app/(app)/records/level/[level]/page.tsx
@@ -7,34 +7,36 @@ import RecordDB, { type MaxRecord } from "@/server/prisma/record.db";
 import AuthUtil from "@/server/utils/auth-util";
 import { notFound } from "next/navigation";
 import SelectLevel from "../../selet-level";
+import LevelBallSC from "@/components/level-ball.server";
+import Link from "next/link";
+import { ChartType } from "@prisma/client";
+import { get } from "http";
 
-async function getSongs(level: number) {
+async function getRecord(
+  userSeq: number,
+  level: number,
+  chartType?: ChartType
+) {
+  if (!chartType) return [];
+
   const allSongs = await ChartDB.findAllGroupBySong();
   const targetSongs = allSongs
-    .filter((song) => song.charts?.find((chart) => chart.level === level))
+    .filter((song) =>
+      song.charts?.find((chart) => {
+        let filter = chart.level === level;
+        if (chartType && chart.chartType !== chartType) filter = false;
+        return filter;
+      })
+    )
     .map((song) => ({
       ...song,
-      charts: song.charts?.filter((chart) => chart.level === level),
+      charts: song.charts?.filter((chart) => {
+        let filter = chart.level === level;
+        if (chartType && chart.chartType !== chartType) filter = false;
+        return filter;
+      }),
     }));
-  return targetSongs;
-}
 
-type Props = {
-  params: {
-    level: string;
-  };
-};
-
-export default async function LevelRecordPage({
-  params: { level: _level },
-}: Props) {
-  const userSeq = await AuthUtil.getUserSeqThrows();
-  const targetLevel = Number(_level);
-  if (isNaN(targetLevel)) {
-    notFound();
-  }
-
-  const targetSongs = await getSongs(targetLevel);
   const chartSeqs = targetSongs
     .map((song) => song.charts?.map((chart) => chart.seq) ?? [])
     .flat();
@@ -58,46 +60,106 @@ export default async function LevelRecordPage({
       return bScore - aScore;
     });
 
+  return songWithRecord;
+}
+
+type Props = {
+  params: {
+    level: string;
+  };
+  searchParams: {
+    CHART_TYPE?: ChartType;
+  };
+};
+
+export default async function LevelRecordPage({
+  params: { level: _level },
+  searchParams: { CHART_TYPE },
+}: Props) {
+  const userSeq = await AuthUtil.getUserSeqThrows();
+  const targetLevel = Number(_level);
+  if (isNaN(targetLevel)) {
+    notFound();
+  }
+
+  const records = await getRecord(userSeq, targetLevel, CHART_TYPE);
+
   return (
     <ContentBox title="내 기록">
       <SelectLevel targetLevel={targetLevel} />
 
-      <div className="flex flex-row justify-center items-center gap-1 flex-wrap">
-        {songWithRecord.map((song) => (
-          <div
-            key={song.seq}
+      <div className="flex flex-row gap-4">
+        <Link href={`/records/level/${targetLevel}?CHART_TYPE=SINGLE`}>
+          <LevelBallSC
             className={classnames(
-              "flex size-14 sm:size-20 md:size-24 ounded-md bg-base-200 justify-center items-center flex-col",
-              "hover:bg-gray-500 active:bg-gray-500 transition-colors"
+              "size-12 sm:size-20 cursor-pointer bg-red-500",
+              CHART_TYPE === "SINGLE" ? "opacity-30" : ""
             )}
-          >
-            <span className="text-center text-[8px] sm:text-xs max-w-full text-ellipsis overflow-hidden whitespace-nowrap">
-              {song.name}
-            </span>
-            {song.charts?.map((chart) => (
-              <div
-                key={chart.seq}
-                className="text-center text-[10px] sm:text-base flex flex-col justify-center items-center gap-1"
-              >
-                <span>{recordMap.get(chart.seq)?.score}</span>
-                {chart.record?.grade && (
-                  <RecordGrade
-                    className="text-[10px] sm:text-xs"
-                    grade={chart.record.grade}
-                    isBreakOff={false}
-                  />
-                )}
-                {chart.record?.plate && (
-                  <RecordPlate
-                    className="text-[10px] sm:text-xs"
-                    plate={chart.record.plate}
-                  />
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
+            text="싱글"
+            hasHover
+          />
+        </Link>
+
+        <Link href={`/records/level/${targetLevel}?CHART_TYPE=DOUBLE`}>
+          <LevelBallSC
+            className={classnames(
+              "size-12 sm:size-20 cursor-pointer bg-green-500",
+              CHART_TYPE === "DOUBLE" ? "opacity-30" : ""
+            )}
+            text="더블"
+            hasHover
+          />
+        </Link>
       </div>
+
+      {CHART_TYPE ? (
+        <div className="flex flex-row justify-center items-center gap-1 flex-wrap">
+          {records.map((song) => (
+            <div
+              key={song.seq}
+              className={classnames(
+                "flex size-14 sm:size-20 md:size-24 ounded-md bg-base-200 justify-center items-center flex-col",
+                "hover:bg-gray-500 active:bg-gray-500 transition-colors px-1"
+              )}
+            >
+              <span className="text-center text-[8px] sm:text-xs max-w-full text-ellipsis overflow-hidden whitespace-nowrap">
+                {song.name}
+              </span>
+              {song.charts?.map((chart) => (
+                <div
+                  key={chart.seq}
+                  className="w-full text-center text-[10px] sm:text-base flex flex-col justify-center items-center gap-1"
+                >
+                  <div className="flex flex-rowjustify-center items-center gap-1">
+                    {chart.record?.score && (
+                      <span className="text-[10px] sm:text-xs">
+                        {chart.record.score}
+                      </span>
+                    )}
+
+                    {chart.record?.grade && (
+                      <RecordGrade
+                        className="text-[8px] sm:text-xs"
+                        grade={chart.record.grade}
+                        isBreakOff={false}
+                      />
+                    )}
+                  </div>
+
+                  {chart.record?.plate && (
+                    <RecordPlate
+                      className="text-[10px] sm:text-xs max-w-full px-1 text-ellipsis overflow-hidden whitespace-nowrap"
+                      plate={chart.record.plate}
+                    />
+                  )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>싱글 더블을 선택해주세요</p>
+      )}
     </ContentBox>
   );
 }
--- src/components/level-ball.server.tsx
+++ src/components/level-ball.server.tsx
@@ -3,14 +3,20 @@ import { Chart } from "@prisma/client";
 import { ButtonHTMLAttributes } from "react";
 
 type Props = {
-  chart: Chart;
+  chart?: Chart;
   hasHover?: boolean;
+  text?: string;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 /**
  * 레벨 볼 서버 컴포넌트
  */
-export default function LevelBallSC({ chart, hasHover, className }: Props) {
+export default function LevelBallSC({
+  chart,
+  hasHover,
+  className,
+  text,
+}: Props) {
   return (
     <div
       className={classnames(
@@ -18,18 +24,20 @@ export default function LevelBallSC({ chart, hasHover, className }: Props) {
         "flex justify-center items-center",
         "ring-2 ring-offset-1 ring-black",
         "transition-opacity",
-        {
-          "bg-red-500": chart.chartType === "SINGLE",
-          "bg-green-500": chart.chartType === "DOUBLE",
-          "bg-yellow-500": chart.chartType === "CO_OP",
-        },
+        chart
+          ? {
+              "bg-red-500": chart.chartType === "SINGLE",
+              "bg-green-500": chart.chartType === "DOUBLE",
+              "bg-yellow-500": chart.chartType === "CO_OP",
+            }
+          : "",
         {
           "hover:opacity-30 active:opacity-30": Boolean(hasHover),
         },
         className ?? ""
       )}
     >
-      {chart.level}
+      {chart ? chart.level : text}
     </div>
   );
 }
