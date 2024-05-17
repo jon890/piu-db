111d465937e158fabd31df9bd7c0bdb89e49567a
Kim Byoung Tae <jon89071@gmail.com>
2024-05-17 14:15:39 +0900

    :lipstick: 기록 모달 UI 수정 (#34)

--- src/app/(app)/records/[record_seq]/page.tsx
+++ src/app/(app)/records/[record_seq]/page.tsx
@@ -4,6 +4,9 @@ import RecordGrade from "@/components/record/record-grade";
 import RecordPlate from "@/components/record/record-plate";
 import ChartDB from "@/server/prisma/chart.db";
 import RecordDB from "@/server/prisma/record.db";
+import TimeUtil from "@/server/utils/time-util";
+import classnames from "@/utils/classnames";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -21,47 +24,82 @@ export default async function RecordDetailPage({
     : await RecordDB.findBySeq(recordSeq);
   const songWithChart = record
     ? await ChartDB.findSongBySeq(record?.chartSeq)
-    : null;
+    : notFound();
+
+  const { chart, song } = songWithChart;
+
+  if (!record || !song || !chart) {
+    return notFound();
+  }
 
   return (
     <ContentBox title="기록 상세">
-      <div className="modal-box space-y-5">
-        <h3 className="font-semibold text-base">{songWithChart?.song?.name}</h3>
-        <div className="flex flex-row justify-between px-16 items-center">
-          {songWithChart && songWithChart.chart && (
-            <LevelBall chart={songWithChart?.chart} className="size-12" />
-          )}
-          <div className="flex flex-col items-center">
-            {record && (
-              <RecordGrade
-                grade={record.grade}
-                isBreakOff={record.isBreakOff}
+      <div className="modal-box p-0 border-4">
+        <div
+          className={classnames("card p-0", song.imageUrl ? "image-full" : "")}
+        >
+          {song.imageUrl && (
+            <figure className="relative opacity-90">
+              <Image
+                src={song.imageUrl}
+                alt={song.name}
+                fill
+                priority={false}
+                sizes="50vw"
               />
+            </figure>
+          )}
+
+          <div className="card-body space-y-4">
+            <h2 className="card-title text-black dark:text-white">
+              {song.name}
+            </h2>
+            <div className="flex flex-row justify-between px-12 items-center">
+              <LevelBall
+                chart={chart}
+                className="size-12 text-black dark:text-white"
+              />
+              <div className="flex flex-col items-center">
+                <RecordGrade
+                  grade={record.grade}
+                  isBreakOff={record.isBreakOff}
+                />
+                <span className="text-black dark:text-white">
+                  {record.score}
+                </span>
+              </div>
+              {record?.plate && <RecordPlate plate={record.plate} />}
+            </div>
+
+            <div className="flex flex-row justify-between px-3 bg-black bg-opacity-75 text-center p-4 rounded-md">
+              <div className="flex flex-col">
+                <span className="text-blue-500 font-semibold">PERFECT</span>
+                <span className="text-white">{record?.perfect ?? 0}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-green-500 font-semibold">GREAT</span>
+                <span className="text-white">{record?.great ?? 0}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-yellow-500 font-semibold">GOOD</span>
+                <span className="text-white">{record?.good ?? 0}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-violet-500 font-semibold">BAD</span>
+                <span className="text-white">{record?.bad ?? 0}</span>
+              </div>
+              <div className="flex flex-col">
+                <span className="text-red-500 font-semibold">MISS</span>
+                <span className="text-white">{record?.miss ?? 0}</span>
+              </div>
+            </div>
+
+            {record.playedAt && (
+              <span className="text-xs text-end text-black dark:text-white">
+                플레이 시간:{" "}
+                {TimeUtil.format(record.playedAt, "YYYY-MM-DD hh:mm:ss")}
+              </span>
             )}
-            {record && <span>{record.score}</span>}
-          </div>
-          {record?.plate && <RecordPlate plate={record.plate} />}
-        </div>
-        <div className="flex flex-row justify-between px-3 bg-slate-500 text-center p-4 rounded-md">
-          <div className="flex flex-col">
-            <span className="text-blue-500 font-semibold">PERFECT</span>
-            <span className="text-white">{record?.perfect ?? 0}</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-green-500 font-semibold">GREAT</span>
-            <span className="text-white">{record?.great ?? 0}</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-yellow-500 font-semibold">GOOD</span>
-            <span className="text-white">{record?.good ?? 0}</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-violet-500 font-semibold">BAD</span>
-            <span className="text-white">{record?.bad ?? 0}</span>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-red-500 font-semibold">MISS</span>
-            <span className="text-white">{record?.miss ?? 0}</span>
           </div>
         </div>
       </div>
